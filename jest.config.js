ee8a30418c1cee29e3a5c5150e31fceaf93c4f66
sw1tchdev <70477902+sw1tchdev@users.noreply.github.com>
2023-11-10 19:55:39 +0700

    feat: init project
    ref: #1

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testEnvironment: 'node',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+};
